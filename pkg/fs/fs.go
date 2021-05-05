01932b3ee3d1de8c14b12de28474d0113671eda5
sebbe <13065474+sebastianappler@users.noreply.github.com>
2021-05-05 22:52:29 +0200

    Refactor merged packages
    This commit merges files together to
    one file instead of spreading the
    packages across multiple. It's more
    in line with a go structures.

--- pkg/fs/fs.go
+++ pkg/fs/fs.go
@@ -1,9 +1,12 @@
 package fs
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"time"
 )
 
@@ -45,4 +48,38 @@ func ReadFile(fullPath string) ([]byte, error) {
 	}
 
 	return data, nil
+}
+
+func Send(fileName string, fileData []byte, path string) error {
+	to := filepath.Join(path, fileName)
+
+	out, err := os.Create(to)
+	if err != nil {
+		return fmt.Errorf("couldn't open dest file: %v\n", err)
+	}
+
+	defer out.Close()
+
+	reader := bytes.NewReader(fileData)
+	_, err = io.Copy(out, reader)
+	if err != nil {
+		return fmt.Errorf("writing to output file failed: %v\n", err)
+	}
+
+	err = out.Sync()
+	if err != nil {
+		return fmt.Errorf("Sync error: %v\n", err)
+	}
+
+	fmt.Printf("file sent: %v\n", fileName)
+	return nil
+}
+
+func Remove(fileName string, path string) error {
+	err := os.Remove(filepath.Join(path, fileName))
+	if err != nil {
+		return fmt.Errorf("unable to remove file: %v\n", err)
+	}
+
+	return nil
 }
